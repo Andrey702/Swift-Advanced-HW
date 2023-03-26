bec1a2691bab0240d725ee31e6799e1d0d6be725
Andrey702 <kotelnikov.andrey702@gmail.com>
2023-03-27 00:27:53 +0300

    add HW7

--- KotelnikovIOSCP4_HW7_1.playground/Contents.swift
+++ KotelnikovIOSCP4_HW7_1.playground/Contents.swift
@@ -14,11 +14,24 @@ import UIKit
  В реализацию метода “исполнить трек” добавьте вывод в консоль “Я (имя артиста) исполнил трек (название трека)”;
  Создайте 3 сабкласса любых артистов и переопределите в них МЕТОДЫ суперкласса класса.
  
+
+ 
+ Задача 2
+ 
+ Создание списка артистов.
+
+ Алгоритм выполнения
+
+ Доработайте существующих артистов так, чтобы они имели уникальные для каждого из них свойства и методы.
+ Защитите этих артистов от редактирования в будущем.
+ 
+
+
  */
 
 
 class Artist {
-    
+     
     var name: String
     var country: String
     var genre: String
@@ -44,30 +57,67 @@ class Artist {
     
 }
 
+
+
+
 class ArtistOne: Artist {
+    
+    final var yearOfFoundation: Int
+    
+    init(name: String, country: String, genre: String, trackName: String, yearOfFoundation: Int) {
+        self.yearOfFoundation = yearOfFoundation
+        super.init(name: name, country: country, genre: genre, trackName: trackName)
+    }
+    
+    
+    final override func writeATrack() {
+        print("Я \(name) написал новый трек \(trackName)")
+    }
+    
+    final override func performATrack() {
+        print("Я \(name) исполнил новый трек \(trackName)")
+    }
     
 }
 
 
+class ArtistTwo: Artist {
+    
+    final var numberOfConcertsPlayed: Int
+    
+    init(name: String, country: String, genre: String, trackName: String, numberOfConcertsPlayed: Int) {
+        self.numberOfConcertsPlayed = numberOfConcertsPlayed
+        super.init(name: name, country: country, genre: genre, trackName: trackName)
+    }
+    
+    
+    final override func writeATrack() {
+        print("Я \(name) написал новый трек \(trackName)")
+    }
+    
+    final override func performATrack() {
+        print("Я \(name) исполнил новый трек \(trackName)")
+    }
+    
+}
 
 
-
-
-
-
-
-
-
-
-/*
- 
- Задача 2
- Создание списка артистов.
-
- Алгоритм выполнения
-
- Доработайте существующих артистов так, чтобы они имели уникальные для каждого из них свойства и методы.
- Защитите этих артистов от редактирования в будущем.
- 
-
- */
+class ArtistThree: Artist {
+    
+    final var ageOfTheArtist: Int
+    
+   init(name: String, country: String, genre: String, trackName: String, ageOfTheArtist: Int) {
+        self.ageOfTheArtist = ageOfTheArtist
+        super.init(name: name, country: country, genre: genre, trackName: trackName)
+    }
+    
+    
+    final override func writeATrack() {
+        print("Я \(name) написал новый трек \(trackName)")
+    }
+    
+    final override func performATrack() {
+        print("Я \(name) исполнил новый трек \(trackName)")
+    }
+    
+}
